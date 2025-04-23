a3889989b1849c6c7fb7418da307e413dbc90387
rin22-dot <nurindahnm7@gmail.com>
2025-04-23 15:24:43 +0700

    Rin-apps

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("🧁 Welcome 🧁")
 st.write(
-    "test website [docs.streamlit.io](https://docs.streamlit.io/)."
+    "test website"
 )
 st.image("239e46c7a088b819d1b9ff9ecd7e5eff.jpg")
