3cd499e5b31e25528e2eb9dada07274bf037a6e1
rin22-dot <nurindahnm7@gmail.com>
2025-05-21 15:15:10 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.write(
 st.write("sIigmAaA")
 st.image("239e46c7a088b819d1b9ff9ecd7e5eff.jpg")
 
-st.write("aplikasi sederhana")
+st.title("aplikasi sederhana")
 st.header("aplikasi mengecek nilai genap/ganjil")
 angka = st.number_input("tulis sebuah angka:", value=0, step=1)
 
