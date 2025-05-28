c452bfd364d8d1751b6ae2ab6972304b9bfcecbf
rin22-dot <nurindahnm7@gmail.com>
2025-05-28 16:05:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,14 +6,6 @@ st.title("sigma 😝")
 st.write("sIigmAaA")
 st.image("9769a4cd94b663946f97ec66857f9d6f.jpg", width=200)
 
-st.title("aplikasi sederhana")
-st.header("aplikasi mengecek nilai genap/ganjil")
-angka = st.number_input("tulis sebuah angka:", value=0, step=1)
-
-if (angka % 2) == 0:
-    st.write(f"{angka} adalah bilangan genap")
-else:
-    st.write(f"{angka} adalah bilangan ganjil") 
 st.title("Aplikasi Sederhana")
 
 # Menggunakan layout kolom
